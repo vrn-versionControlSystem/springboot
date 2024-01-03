671fccb253121d971104a3aefa3511c4aaade654
January month <jmonth791@gmail.com>
2024-01-03 11:23:28 +0530

    first commit

---
+++ src/main/java/com/Springboot112024Application.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot112024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot112024Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/repo/UserRepository.java
@@ -0,0 +1,11 @@
+package com.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+	
+	
+}
